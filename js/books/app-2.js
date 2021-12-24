79deb8ef0f6955bd394c8d7201f97e313418bee2
Enzo Speroni <speronienzo20@gmail.com>
2021-12-24 02:39:15 -0300

    Cambiando el index.html por inputs + app-2.js

---
+++ js/books/app-2.js
@@ -0,0 +1,3 @@
+let tituloLibro = document.getElementById("nombreLibro").value;
+let generoLibro = document.getElementById("generoLibro").value;
+let paginasLibro = document.getElementById("paginasLibro").value;
