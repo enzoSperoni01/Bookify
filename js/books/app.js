e86f684f30186410d0ef4c9c7255233f54e1873e
Enzo Speroni <speronienzo20@gmail.com>
2021-12-26 02:31:42 -0300

    Comentando 'app.js' - Es para tenerlo de referencia

--- js/books/app.js
+++ js/books/app.js
@@ -1,38 +1,38 @@
-let añadirLibro;
-let genero;
-let paginas;
+// let añadirLibro;
+// let genero;
+// let paginas;
 
-let listadoLibros = [];
+// let listadoLibros = [];
 
-let nombre = prompt('JS: "Hola usuario, soy JavaScript!\nPor favor, dime ¿Cual es tu nombre?"');
-nombre = capitalize(nombre);
+// let nombre = prompt('JS: "Hola usuario, soy JavaScript!\nPor favor, dime ¿Cual es tu nombre?"');
+// nombre = capitalize(nombre);
 
-if (localStorage.getItem("name") !== nombre) {
-    alert(`JS: "Veo queres nuevo por aqui ${nombre}..."`);
-    localStorage.setItem("name", nombre);
-    let apellido = prompt(`JS: "Dime, ¿Cual es tu apellido?"`);
-    apellido = capitalize(apellido);
-    localStorage.setItem("lastname", apellido);
-    alert(`JS: "Genial ${nombre} ${apellido}, comienza a anotar tus libros!"`);
-} else {
-    let nombreBD = localStorage.getItem("name");
-    alert(`Bienvenido de vuelta ${nombreBD}!\nVe a seguir anotando tus libros favoritos...`);
-}
+// if (localStorage.getItem("name") !== nombre) {
+//     alert(`JS: "Veo queres nuevo por aqui ${nombre}..."`);
+//     localStorage.setItem("name", nombre);
+//     let apellido = prompt(`JS: "Dime, ¿Cual es tu apellido?"`);
+//     apellido = capitalize(apellido);
+//     localStorage.setItem("lastname", apellido);
+//     alert(`JS: "Genial ${nombre} ${apellido}, comienza a anotar tus libros!"`);
+// } else {
+//     let nombreBD = localStorage.getItem("name");
+//     alert(`Bienvenido de vuelta ${nombreBD}!\nVe a seguir anotando tus libros favoritos...`);
+// }
 
-let nombreLibro;
+// let nombreLibro;
 
-while(nombreLibro !== "ESC") {
-    let añadirLibro = prompt("Escribe el nombre del libro:");
-    nombreDelLibro(añadirLibro);
-    let genero = prompt("Cual es el genero del libro?");
-    generoLibro(genero);
-    let paginas = parseInt(prompt("Cuantas paginas contiene?"));
-    cantPaginas(paginas);
-    listadoLibros.push(new Libros(añadirLibro, genero, paginas));
-    ordenarPaginas();
-    let reiniciar = prompt("Quieres añadir otro libro?");
-    añadirOtro(reiniciar);
-}
+// while(nombreLibro !== "ESC") {
+//     let añadirLibro = prompt("Escribe el nombre del libro:");
+//     nombreDelLibro(añadirLibro);
+//     let genero = prompt("Cual es el genero del libro?");
+//     generoLibro(genero);
+//     let paginas = parseInt(prompt("Cuantas paginas contiene?"));
+//     cantPaginas(paginas);
+//     listadoLibros.push(new Libros(añadirLibro, genero, paginas));
+//     ordenarPaginas();
+//     let reiniciar = prompt("Quieres añadir otro libro?");
+//     añadirOtro(reiniciar);
+// }
 
-imprimirLibros();
-console.log(listadoLibros);
+// imprimirLibros();
+// console.log(listadoLibros);
