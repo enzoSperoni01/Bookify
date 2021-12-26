e7ec6eaf08eb373ea3a19534ef9b868c4bca94fb
Enzo Speroni <speronienzo20@gmail.com>
2021-12-26 02:30:32 -0300

    Corriegiendo error de src en <script> => 'index.html'

--- js/books/app-2.js
+++ js/books/app-2.js
@@ -7,7 +7,7 @@ let listadoLibros = [];
 function botonListo() {
     tituloLibro = document.getElementById("nombreLibro").value;
     generoLibro = document.getElementById("generoLibro").value;
-    generoLibro = capitalize(generoLibro)
+    generoLibro = capitalize(generoLibro);
     paginasLibro = document.getElementById("paginasLibro").value;
 
     listadoLibros.push(new Libros(tituloLibro, generoLibro, paginasLibro));
