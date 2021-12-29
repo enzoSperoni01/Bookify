8f87786d2ece7e3c05136fab79abd86399e9baea
Enzo Speroni <speronienzo20@gmail.com>
2021-12-28 22:08:27 -0300

    Boton para Eliminar los 'item-list'

--- js/books/functions.js
+++ js/books/functions.js
@@ -14,22 +14,22 @@ function ordenarPaginas() {
         return 0;
     });
 }
-function imprimirLibros(){
+function imprimirLibros() {
     let html = "";
-    listadoLibros.forEach(function(book){            
-        html += `<li data-aos="fade-up" data-aos-duration="1000">
+    let i = 0;
+    listadoLibros.forEach(function(book) {
+        html += `<li id="libro-${i}" class="section__div__list__item" data-aos="fade-up" data-aos-duration="600">
                     Titulo: ${book.libro},<br>
                     Genero: ${book.genero},<br>
                     Paginas: ${book.paginas}<br>
-                    <button id="botonEliminarLibro">Eliminar</button>
+                    <button id="boton-borrar" class="section__div__list__item__btn" onclick="eliminarLibro(${i})">Eliminar</button>
                 </li>`;
+        i++;
     });
     document.getElementById("biblioteca").innerHTML = html;
 }
-function eliminarItem(){
-    let botonEliminar = document.getElementById("botonEliminarLibro");
-    botonEliminar.addEventListener("click", () => {
-        let listaOl = document.getElementById("biblioteca");
-        listaOl.removeChild();
-    });
+function eliminarLibro(num) {
+    let libroBorrar = document.getElementById(`libro-${num}`);
+    libroBorrar.remove();
+    listadoLibros.splice(num, 1);
 }
