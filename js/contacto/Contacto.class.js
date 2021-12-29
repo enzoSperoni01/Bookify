b70834639f2674655c8c6caf4322c81bd1d6f10f
Enzo Speroni <speronienzo20@gmail.com>
2021-12-29 19:34:45 -0300

    Comentando 'Contacto.class.js' => Que hace la clase y el metodo '.toUpperCase()'

--- js/contacto/Contacto.class.js
+++ js/contacto/Contacto.class.js
@@ -1,5 +1,8 @@
+// La clase que nos creara los objetos de cada Contacto.
+// Que se enviaran y crearan en el archivo 'functions.js'
 class Contacto {
     constructor(fullName, correo, mensaje){
+        // El .toUpperCase() para que el nombre y apellido ingresado en el input este en Mayuscula
         this.nombre = fullName.toUpperCase();
         this.correo = correo;
         this.mensaje = mensaje;
