49575cca25dc38c96ef99a21d1737b69bd20ca83
Enzo Speroni <speronienzo20@gmail.com>
2021-12-29 19:21:38 -0300

    Comentando 'Libros.class.js' => Explicando algunos codigos

--- js/books/Libros.class.js
+++ js/books/Libros.class.js
@@ -1,7 +1,14 @@
+// Esto generara cada libro con sus propios valores dentro
+// de las propiedades
 class Libros {
     constructor(libro, genero, paginas) {
-        this.libro = capitalize(libro);
+        /* El "capitalize" es en caso de que, si el usuario los escribe
+        en minuscula(tanto el titulo como el genero), que los devuelva
+        impresos en el DOM con mayuscula en la primera letra */
+        this.libro = capitalize(libro); 
         this.genero = capitalize(genero);
+        // parseInt() para asegurarse de que lo que ingresa es un "numero"
+        // Y no un "string"
         this.paginas = parseInt(paginas);
     }
 }
