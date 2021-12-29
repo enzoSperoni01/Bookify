60e37007bb9672c5d844a740050b538987fa70d3
Enzo Speroni <speronienzo20@gmail.com>
2021-12-29 19:37:35 -0300

    Comentando 'mensajeRecibido.js' => Explicando donde esta anidada la funcion y que es lo que hace

--- js/contacto/mensajeRecibido.js
+++ js/contacto/mensajeRecibido.js
@@ -1,5 +1,7 @@
+// Esta funcion esta directamente anidada en el <body> del archivo 'contactoHecho.html'
 function redirigirContacto() {
     setTimeout(() => {
+        // Una vez pasan los 5000 (5000 == 5s), devuelve al usuario al archivo 'contacto.html'
         window.location.href = "../pages/contacto.html";
     }, 5000);
 }
