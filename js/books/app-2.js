cd450cfbb68474a6c41ad46e848d0ffe8afdeaf2
Enzo Speroni <speronienzo20@gmail.com>
2021-12-29 19:11:33 -0300

    Comentando 'app-2.js' => Explicando por partes

--- js/books/app-2.js
+++ js/books/app-2.js
@@ -2,15 +2,19 @@ let tituloLibro;
 let generoLibro;
 let paginasLibro;
 
+// Array donde se almacenan los libros,
+// escritos en los input de "index.html"
 let listadoLibros = [];
 
 function botonListo() {
+    // Obtenemos el valor de cada input del "index.html"
     tituloLibro = document.getElementById("nombreLibro").value;
     generoLibro = document.getElementById("generoLibro").value;
-    generoLibro = capitalize(generoLibro);
     paginasLibro = document.getElementById("paginasLibro").value;
 
+    // Creamos una constante que contenga el objeto creado
     const nuevoLibro = new Libros(tituloLibro, generoLibro, paginasLibro);
+    // Y enviamos al array el objeto creado
     listadoLibros.push(nuevoLibro);
     ordenarPaginas();
     imprimirLibros();
