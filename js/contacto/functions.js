7e8d9f670ede23cc364def990ea336abe875c843
Enzo Speroni <speronienzo20@gmail.com>
2022-01-07 03:04:10 -0300

    Agregando jQuery al 'contacto.html' => Un mensaje de ERROR

--- js/contacto/functions.js
+++ js/contacto/functions.js
@@ -4,27 +4,37 @@ function enviarContacto() {
     nombreApellido = $("#nombreApellido")[0].value;
     correoElectronico = $("#correoElectronico")[0].value;
     mensaje = $("#comentarios")[0].value;
+    
+    if(nombreApellido && correoElectronico && mensaje !== "") {
+        // Creamos un objeto con la clase "Contacto", y le pasamos los valores de los inputs
+        contactoDeUsuario = new Contacto(nombreApellido, correoElectronico, mensaje);
+    
+        /* Almacenamos en el localStorage la variable contactoDeUsuario
+         * Con la key 'datos-*correoElectronico*', Esto para que cada key sea unico segun el correo        ingresado
+         * Y el objeto contactoDeUsuario lo pasamos a un JSON pero en string 
+        */ 
 
-    // Creamos un objeto con la clase "Contacto", y le pasamos los valores de los inputs
-    contactoDeUsuario = new Contacto(nombreApellido, correoElectronico, mensaje);
+        localStorage.setItem(`datos-${correoElectronico}`, JSON.stringify(contactoDeUsuario));
+        let boton = $("#button-contact")[0];
 
-    // Almacenamos en el localStorage la variable contactoDeUsuario
-    // Con la key 'datos-*correoElectronico*', Esto para que cada key sea unico segun el correo ingresado
-    // Y el objeto contactoDeUsuario lo pasamos a un JSON pero en string 
-    localStorage.setItem(`datos-${correoElectronico}`, JSON.stringify(contactoDeUsuario));
-
-    let boton = $("#button-contact")[0];
-
-    // Con esta funcion podemos realizar acciones en un tiempo determinado
-    setTimeout(() => {
-        boton.value = "Enviando...";
+        // Con esta funcion podemos realizar acciones en un tiempo determinado
         setTimeout(() => {
-            boton.value = "Enviado!";
+            boton.value = "Enviando...";
             setTimeout(() => {
+                boton.value = "Enviado!";
+                setTimeout(() => {
+                    // Con este metodo podemos dirigir al usuario, como un link de HTML(<a href=""></a>), pero en JS
+                    window.location.href = "../pages/contactoHecho.html";
+                }, 1700);
+            }, 2500);
+        }, 500);
+    } else {
+        $("#button-contact").click(() => {
+            $(".p-alert").fadeIn();
+            setTimeout(() => {
+                $(".p-alert").fadeOut();
+            }, 3500);
+        })
+    }
 
-                // Con este metodo podemos dirigir al usuario, como un link de HTML(<a href=""></a>), pero en JS
-                window.location.href = "../pages/contactoHecho.html";
-            }, 1700);
-        }, 2500);
-    }, 500);
 }
