30ea89d4ec579bb369c38c4ca08ddd7a9f00bc7a
Enzo Speroni <speronienzo20@gmail.com>
2022-01-06 21:15:34 -0300

    Mejorando el archivo 'Swal Alerts'

--- js/Swal/swal_alerts.js
+++ js/Swal/swal_alerts.js
@@ -1,10 +1,18 @@
 function enviarModal() {
-	let mensaje = {
+	let emailRecibido = {
 		title: "Enviado!",
-		text: `Tu email ha sido recibido con exito!`,
+		text: "Tu email ha sido recibido con exito!",
 		icon: "success",
 		confirmButtonText: "Listo!",
 	};
 
-	Swal.fire(mensaje);
+    let emailError = {
+        title: 'Ha habido un error!',
+        text: 'Debes llenar la casilla con tu correo',
+        icon: 'warning'
+    }
+
+    let correo = $("#email-newsletter")[0].value;
+    if(correo !== "") Swal.fire(emailRecibido);
+    else Swal.fire(emailError);
 }
