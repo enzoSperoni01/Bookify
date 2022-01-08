4000e85a5ca98428406929eb7e35220ad5e84d77
Enzo Speroni <speronienzo20@gmail.com>
2022-01-08 14:13:41 -0300

    Mejorando los alerts del Newsletter + Nuevo 'alerts.data.js'

---
+++ js/Swal/alerts.data.js
@@ -0,0 +1,30 @@
+const emailRecibido = {
+    title: "Correo recibido!",
+    text: "Tu email ha sido recibido con exito!",
+    icon: "success",
+    confirmButtonText: "Listo!",
+    timer: 3500,
+    timerProgressBar: true,
+    showConfirmButton: false
+};
+const emailError = {
+    title: '<h3 style="color: white;">Ha habido un error!</h3>',
+    html: '<b style="color: white;">Debes llenar la casilla con tu correo</b>',
+    icon: 'warning',
+    background: 'hsl(210, 45%, 45%)',
+    timer: 3500,
+    timerProgressBar: true,
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false
+}
+const emailRepetido = {
+    title: '<h3>Tu email ya fue registrado</h3>',
+    icon: 'info',
+    background: 'hsl(0, 0%, 90%)',
+    timer: 3500,
+    timerProgressBar: true,
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false
+}
--- js/Swal/swal_alerts.js
+++ js/Swal/swal_alerts.js
@@ -1,24 +1,11 @@
 function enviarModal() {
-	let emailRecibido = {
-		title: "Enviado!",
-		text: "Tu email ha sido recibido con exito!",
-		icon: "success",
-		confirmButtonText: "Listo!",
-	};
-
-    let emailError = {
-        title: '<h3 style="color: white;">Ha habido un error!</h3>',
-        html: '<b style="color: white;">Debes llenar la casilla con tu correo</b>',
-        icon: 'warning',
-        background: 'hsl(210, 45%, 45%)',
-        timer: 3500,
-        timerProgressBar: true,
-        toast: true,
-        position: 'bottom-end',
-        showConfirmButton: false
-    }
-
     let correo = $("#email-newsletter")[0].value;
-    if(correo !== "") Swal.fire(emailRecibido);
-    else Swal.fire(emailError);
+    if(correo !== "" && localStorage.getItem(`newsletter-${correo}`) !== correo){
+        localStorage.setItem(`newsletter-${correo}`, correo);
+        Swal.fire(emailRecibido);
+    } else if(correo !== "" && correo === localStorage.getItem(`newsletter-${correo}`)) {
+        Swal.fire(emailRepetido);
+    } else {
+        Swal.fire(emailError);
+    }
 }
