bc557c1feae60828bc7d4f8eecce5a0c98fda4d7
Enzo Speroni <speronienzo20@gmail.com>
2022-01-11 21:52:06 -0300

    Creando Array de Libros - Aun sin completar

---
+++ js/find-book/books.data.js
@@ -0,0 +1,142 @@
+const listaDeLibros = [
+    {
+        id: 1,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: '' /* Agregar imagenes de C/U */
+    },
+    {
+        id: 2,
+        titulo: "El Resplandor",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 3,
+        titulo: "El Señor de los Anillos: La comunidad del anillo",
+        autor: "Tolkien",
+        genero: "Fantasia",
+        img: ''
+    },
+    {
+        id: 4,
+        titulo: "El Señor de los Anillos: Las dos torres",
+        autor: "Tolkien",
+        genero: "Fantasia",
+        img: ''
+    },
+    {
+        id: 5,
+        titulo: "El Señor de los Anillos: El retorno del rey",
+        autor: "Tolkien",
+        genero: "Fantasia",
+        img: ''
+    },
+    {
+        id: 6,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 7,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 8,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 9,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 10,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 11,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 12,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 13,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 14,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 15,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 16,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 17,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 18,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 19,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    },
+    {
+        id: 20,
+        titulo: "Carrie",
+        autor: "Stephen King",
+        genero: "Terror",
+        img: ''
+    }
+];
