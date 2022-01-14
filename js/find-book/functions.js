fba9e7ea8d7cbe7b82023eb094660e2e32962831
Enzo Speroni <speronienzo20@gmail.com>
2022-01-13 22:09:23 -0300

    Dando mas estilos + Mejorando '_find-book.scss'

--- js/find-book/functions.js
+++ js/find-book/functions.js
@@ -10,8 +10,10 @@ function imprimirLibros(book) {
 						<img src="../assets/img-books/${book.img}.jpg" alt="${book.titulo}" />
 						<div class="info-book">
 							<h3>${book.titulo}</h3>
-							<h3 class="writter-book">${book.autor}</h3>
-							<p>${book.genero}</p>
+							<div class="writer-gender">
+								<h3 class="writter-book">${book.autor}</h3>
+								<p>${book.genero}</p>
+							</div>
 						</div>
 					</div>`);
 }
