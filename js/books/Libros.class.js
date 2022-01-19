24ac3b946a1b0c561ca78ca7544401b8e2042bd5
Enzo Speroni <speronienzo20@gmail.com>
2022-01-19 19:25:44 -0300

    Descomentando => 'js/books/'

--- js/books/Libros.class.js
+++ js/books/Libros.class.js
@@ -2,13 +2,8 @@
 // de las propiedades
 class Libros {
     constructor(libro, genero, paginas) {
-        /* El "capitalize" es en caso de que, si el usuario los escribe
-        en minuscula(tanto el titulo como el genero), que los devuelva
-        impresos en el DOM con mayuscula en la primera letra */
         this.libro = capitalize(libro); 
         this.genero = capitalize(genero);
-        // parseInt() para asegurarse de que lo que ingresa es un "numero"
-        // Y no un "string"
         this.paginas = parseInt(paginas);
     }
 }
--- js/books/main.js
+++ js/books/main.js
@@ -7,14 +7,13 @@ let paginasLibro;
 let listadoLibros = [];
 
 const botonListo = () => {
-    // Obtenemos el valor de cada input del "index.html"
     tituloLibro = $("#nombreLibro")[0].value;
     generoLibro = $("#generoLibro")[0].value;
     paginasLibro = $("#paginasLibro")[0].value;
 
     // Creamos una constante que contenga el objeto creado
     const nuevoLibro = new Libros(tituloLibro, generoLibro, paginasLibro);
-    // Y enviamos al array el objeto creado
+
     listadoLibros.push(nuevoLibro);
     ordenarPaginas();
     imprimirLibros();
